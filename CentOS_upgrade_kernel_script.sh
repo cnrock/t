7da7eec354f69e0591ed90952994ad4b3c7e7daf
WanJie <webmaster@wanjie.info>
2022-04-21 20:00:45 +0800

    Update CentOS_upgrade_kernel_script.sh

--- CentOS_upgrade_kernel_script.sh
+++ CentOS_upgrade_kernel_script.sh
@@ -3,7 +3,10 @@
 # Title: CentOS7.6_x64_upgrade_kernel_script
 # Author: ZNing
 # Date: 2019-7-3 23:13:09
-# Pass on the Aliyun CentOS7.6 x64
+# MOD by cnrock // 2022-04-21
+# 修改选择时的中文注释，修改 elrepo-release-7.el7.elrepo.noarch.rpm 地址
+# 如果是centos 8 ，修改  https://www.elrepo.org/elrepo-release-8.el8.elrepo.noarch.rpm 即可 ，这里未做判断，毕竟8停了
+# Pass on the CentOS 7.* x64
 # You should run this script three times. The first is yum update, the second is kernel upgrade to 5, the final one is optional when you decided to maunal which is deleting old kernel.
 # Having fun to using it. (*^▽^*) Best wishes from Misaka Mikoto and Misaka 10086. 
 
@@ -19,9 +22,9 @@ function upgradeYum()
 function upgradeKernel()
 {
     rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org;
-    rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm;
+    rpm -Uvh http://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm;
     yum --disablerepo="*" --enablerepo="elrepo-kernel" list available;
-    yum --enablerepo=elrepo-kernel install -y kernel-ml;
+    yum --enablerepo=elrepo-kernel install -y kernel-lt;
     grub2-mkconfig -o /boot/grub2/grub.cfg;
     grub2-editenv list;
     awk -F\' '$1=="menuentry " {print i++ " : " $2}' /etc/grub2.cfg;
@@ -61,14 +64,14 @@ function checkSystem()
     uname -sr;
     uname -a;
     echo "[Notice] Confirm Upgrade Kernel? please select: (1~4)"
-    select selected in 'Upgrade Step 1' 'Upgrade Step 2' 'Delete Old Kernel' 'Exit'; do break; done;
+    select selected in '升级所有的包' '仅升级内核至5.*-lt长期版' '删除旧内核' 'Exit'; do break; done;
 	[ "$selected" == 'Exit' ] && echo 'Exit Upgrade.' && exit;
-	if [ "$selected" == 'Upgrade Step 1' ]; then
+	if [ "$selected" == '升级所有的包' ]; then
 		upgradeYum;
-	elif [ "$selected" == 'Upgrade Step 2' ]; then
+	elif [ "$selected" == '仅升级内核至5.*-lt长期版' ]; then
 		upgradeKernel;
         confirmKernel;
-	elif [ "$selected" == 'Delete Old Kernel' ]; then
+	elif [ "$selected" == '删除旧内核' ]; then
 		removeOldKernel;
 	else
 		ConfirmInstall;
